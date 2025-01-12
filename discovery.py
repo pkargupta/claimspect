44eb12fff3710e9a00bea3a9cdb9fed9b1d9c4d4
Priyanka Kargupta <priyankakargupta@gmail.com>
2025-01-12 17:41:22 -0600

    added subaspect discovery + prompts + preprocessing pdf2txt

--- discovery.py
+++ discovery.py
@@ -2,33 +2,7 @@ from vllm import LLM
 from vllm import SamplingParams
 from outlines.serve.vllm import JSONLogitsProcessor
 import json
-
-class gen_subaspect_schema(BaseModel):
-    subaspect_label: Annotated[str, StringConstraints(strip_whitespace=True)]
-    subaspect_keywords: Annotated[str, StringConstraints(strip_whitespace=True)]
-    
-class subaspect_list_schema(BaseModel):
-    subaspect_list : conlist(gen_subaspect_schema, min_length=2, max_length=5)
-
-subaspect_instruction = lambda topic, k=5, n=10: f"You are an analyst that identifies the different aspects (a minimum of 2 and a maximum of {k} aspects) that scientific research papers would consider when determining the answer to the following topic: \"{topic}\". For each aspect, provide {n} additional keywords that would typically be used to describe that aspect for the topic, {topic}.\n\n"
-
-def subaspect_prompt(topic, k=5, n=10): 
-    prompt = aspect_candidate_init_prompt(topic, k, n)
-    prompt += f"""For the topic, {topic}, output the list of up to {k} aspects in the following JSON format:
----
-{{
-    "aspect_list":
-        [
-            {{
-                "aspect_label": <should be a brief, 5-10 word string where the value is an all-lowercase label of the aspect (phrase-length)>,
-                "aspect_keywords": <list of {n} unique, all-lowercase, and comma-separated keywords (always a space after the comma) commonly used to describe the aspect_label (e.g., keyword_1, keyword_2, ..., keyword_{n})>
-            }}
-        ]
-}}
----
-"""
-
-    return prompt
+from prompts import subaspect_list_schema, subaspect_prompt
 
 def subaspect_discovery(args, segments, rank2id, parent_aspect, top_k=10):
 
@@ -37,13 +11,13 @@ def subaspect_discovery(args, segments, rank2id, parent_aspect, top_k=10):
     logits_processor = JSONLogitsProcessor(schema=subaspect_list_schema, llm=args.model.llm_engine)
     sampling_params = SamplingParams(max_tokens=2000, logits_processors=[logits_processor], temperature=temperature, top_p=top_p)
 
-    output = args.model.generate(subaspect_prompt(claim), sampling_params=sampling_params)[0].outputs[0].text
+    output = args.model.generate(subaspect_prompt(parent_aspect.name, args.claim, subset), sampling_params=sampling_params)[0].outputs[0].text
 
-    aspects = json.loads(output)['subaspect_list']
+    subaspects = json.loads(output)['subaspect_list']
     
-    print(f"Generated coarse-grained aspects for claim '{claim}': {aspects}")
+    print(f"Generated subaspects for parent aspect '{parent_aspect.name}': {subaspects}")
     
-    return
+    return subaspects
 
 
 def perspective_discovery():
--- prompts.py
+++ prompts.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel, StringConstraints, conlist
 from typing_extensions import Annotated
 
+############ COARSE-GRAINED ASPECT DISCOVERY ############
+
 class gen_aspect_schema(BaseModel):
     aspect_label: Annotated[str, StringConstraints(strip_whitespace=True)]
     aspect_keywords: Annotated[str, StringConstraints(strip_whitespace=True)]
@@ -21,6 +23,39 @@ def aspect_prompt(topic, k=5, n=10):
             {{
                 "aspect_label": <should be a brief, 5-10 word string where the value is an all-lowercase label of the aspect (phrase-length)>,
                 "aspect_keywords": <list of {n} unique, all-lowercase, and comma-separated keywords (always a space after the comma) commonly used to describe the aspect_label (e.g., keyword_1, keyword_2, ..., keyword_{n})>
+            }}
+        ]
+}}
+---
+"""
+
+    return prompt
+
+############ SUBASPECT DISCOVERY ############
+
+class gen_subaspect_schema(BaseModel):
+    subaspect_label: Annotated[str, StringConstraints(strip_whitespace=True)]
+    subaspect_keywords: Annotated[str, StringConstraints(strip_whitespace=True)]
+    
+class subaspect_list_schema(BaseModel):
+    subaspect_list : conlist(gen_subaspect_schema, min_length=2, max_length=5)
+
+subaspect_instruction = lambda aspect, topic, k, n: f"You are an analyst that identifies different subaspects of parent aspect, {aspect}, to consider when evaluating whether or not {topic}. You will identify a minimum of 2 and a maximum of {k} subaspects that the provided set of scientific research paper excerpts are considering when determining the answer to the following topic: {aspect} for \"{topic}\". For each subaspect, provide {n} additional keywords that would typically be used to describe that subaspect, using the research excerpts we provide you as reference.\n\n"
+
+def subaspect_prompt(aspect, topic, segments, k=5, n=10): 
+    prompt = subaspect_instruction(aspect, topic, k, n)
+
+    for idx, segment in enumerate(segments):
+        prompt += f"Research Paper Excerpt #{idx+1}: {segment}\n\n"
+    
+    prompt += f"""For the topic, {topic}, output the list of up to {k} subaspects of parent aspect {aspect} in the following JSON format:
+---
+{{
+    "subaspect_list":
+        [
+            {{
+                "subaspect_label": <should be a brief, 5-10 word string where the value is an all-lowercase label of the subaspect (phrase-length) that falls under parent aspect {aspect}>,
+                "subaspect_keywords": <list of {n} unique, all-lowercase, and comma-separated keywords (always a space after the comma) used to describe the subaspect_label (e.g., keyword_1, keyword_2, ..., keyword_{n}) based on the input excerpts>
             }}
         ]
 }}
