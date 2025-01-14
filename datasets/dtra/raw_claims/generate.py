69a89446b35becc1c27d2757f46f293a5f9d690a
Rachum-thu <369933193@qq.com>
2025-01-14 15:09:31 -0600

    modify the prompt and update the results

--- datasets/dtra/raw_claims/generate.py
+++ datasets/dtra/raw_claims/generate.py
@@ -2,16 +2,29 @@ import os
 import json
 from api.openai.chat import chat
 
-CLAIM_GENERATION_PROMPT = """
+POS_CLAIM_GENERATION_PROMPT = """
 Scientific or political claims are often nuanced and multifaceted, rarely lending themselves to simple “yes” or “no” answers. To answer such questions effectively, claims must be broken into specific aspects for in-depth analysis, with evidence drawn from relevant scientific literature. We are currently studying such claims using this corpus: 
 {context}
-Task: Generate 20 nuanced and diverse claims based on this corpus. The claims should adhere to the following criteria:
-	1.	Diversity: The claims should be sufficiently varied: (i) they should involve diverse sub-topics in the context; (ii) they should come from different perspectives (not always from the author's side).
+Task: Generate 10 nuanced and diverse claims based on this corpus. The claims should adhere to the following criteria:
+	1.	Diversity: The claims should be sufficiently varied: they should involve diverse sub-topics in the context
 	2.	Complexity: The claims should be complex and controversial (and not necessarity true), requiring multi-aspect analysis rather than simplistic treatment. Avoid overly straightforward or simplistic claims.
 	3.	Research Feasibility: The claims should not be too specific and should pertain to topics with a likely body of existing literature to support evidence-based exploration.
     4.  Concision: The claims should be concise and focused in one short sentence.
     5.  Completeness: The claims should be complete and not require additional context to understand.
 Output: Provide the claims as a list."""
+
+NEG_CLAIM_GENERATION_PROMPT = """
+Political claims are often nuanced and multifaceted, rarely lending themselves to simple “yes” or “no” answers. To answer such questions effectively, claims must be broken into specific aspects for in-depth analysis, with evidence drawn from relevant scientific literature. And they are often controversial. We are currently studying such claims using this corpus: 
+{context}
+Task: Generate 10 nuanced and diverse claims AGAINST on this corpus. The claims should adhere to the following criteria:
+	1.	Diversity: The claims should be sufficiently varied: they should involve diverse sub-topics in the context
+	2.	Complexity: The claims should be complex and controversial (and not necessarity true), requiring multi-aspect analysis rather than simplistic treatment. Avoid overly straightforward or simplistic claims.
+	3.	Research Feasibility: The claims should not be too specific and should pertain to topics with a likely body of existing literature to support evidence-based exploration.
+    4.  Concision: The claims should be concise and focused in one short sentence.
+    5.  Completeness: The claims should be complete and not require additional context to understand.
+Output: Provide the claims as a list.
+    6.	The claims should be against the point of view in the context.
+"""
 
 ORGANIZE_LIST_PROMPT = """
 Please organize the following list of claims into a python list (without the index) like ["claim1", "claim2", "claim3", ...]. You must not generate other content like ```python sign.
@@ -41,9 +54,10 @@ def main():
     output_paths = get_json_paths(txt_paths)
     
     txt_list = load_txt_files(txt_paths)
-    raw_responses = chat([CLAIM_GENERATION_PROMPT.format(context=txt_str) for txt_str in txt_list], model_name='gpt-4o',seed=42)
+    raw_responses_pos = chat([POS_CLAIM_GENERATION_PROMPT.format(context=txt_str) for txt_str in txt_list], model_name='gpt-4o',seed=42)
+    raw_responses_neg = chat([NEG_CLAIM_GENERATION_PROMPT.format(context=txt_str) for txt_str in txt_list], model_name='gpt-4o',seed=42)
+    organized_responses = chat([ORGANIZE_LIST_PROMPT.format(claim_list=pos+neg) for pos, neg in zip(raw_responses_pos, raw_responses_neg)], model_name='gpt-4o-mini',seed=42)
     
-    organized_responses = chat([ORGANIZE_LIST_PROMPT.format(claim_list=raw_response) for raw_response in raw_responses], model_name='gpt-4o-mini',seed=42)
     # convert organized_responses to json
     organized_json = []
     for organized_response in organized_responses:
